ee43dcd63d38e38e187b7f3cc048c4f98fe2d639
GaiusB <gaius.bougna@epitech.eu>
2024-02-21 19:19:46 +0100

    feat: hoovermouse done + arithmetitic symboles clickable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
   title = 'Gaïus-ang-calculator';
 
   /* lets creat some variables */
 
-  calcValue: number = 0;
+  calValue: number = 0
   funcT: any = 'NoFunction'
   /* the aboce variable a created to assigne the current value and the function of the cal  */
 
